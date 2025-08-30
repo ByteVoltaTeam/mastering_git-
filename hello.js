a07e608f225301247366635dd5fed376e5a07688
Joao Pedro <joaopedrovieirasilveira826@gmail.com>
2025-08-30 17:40:42 -0300

    New console.log

--- hello.js
+++ hello.js
@@ -1,6 +1,9 @@
 console.log('Hello, Git!');
 console.log('Hello, GitHub!');
 
+console.log('Hello, I just finished the course of using Git');
+//XD DX 
+
 // I save the team 
 
 // I'm  in the process of implementing an important feature
